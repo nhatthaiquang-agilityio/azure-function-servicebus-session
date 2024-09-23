f82628e496088378f4b4e48d1ec8c3f92bac52a2
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-23 14:44:41 +0700

    Update packages

---
+++ ServiceBusSenderMessages/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var connectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
+var queueName = Environment.GetEnvironmentVariable("QueueName");
+
+int sessions = 10;
+int messagePerSession = 2;
+
+Console.WriteLine("Creating Service Bus sender....");
+var taskList = new List<Task>();
+var sender = new MessageSender(connectionString, queueName);
+
+for(int s = 0; s < sessions; s++)
+{
+    var sessionId = $"Patient-{s.ToString()}";
+    var messageList = new List<Message>();
+
+    for(int m = 0; m < messagePerSession; m++)
+    {
+        var message = new Message(Encoding.UTF8.GetBytes($"Message-{m}"))
+        {
+            SessionId = sessionId
+        };
+        messageList.Add(message);
+    }
+
+    taskList.Add(sender.SendAsync(messageList));
+}
+
+Console.WriteLine("Sending all messages...");
+
+await Task.WhenAll(taskList);
+
+Console.WriteLine("All messages sent.");
